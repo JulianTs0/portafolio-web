cedc9cae9ac416d0a285902fb9f4ea085dbdcbf0
JulianTs0 <juliantorresssr@gmail.com>
2025-12-24 13:00:26 -0300

    update: main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './components/App.tsx'
+import RoutesManager from './ui/routes/routes-manager'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RoutesManager />
+    </StrictMode>,
 )
