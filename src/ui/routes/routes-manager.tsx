c873bf268d943976cd6a53e9e0f34d55d87b0965
JulianTs0 <juliantorresssr@gmail.com>
2026-01-06 17:46:11 -0300

    fix: other routes

--- src/ui/routes/routes-manager.tsx
+++ src/ui/routes/routes-manager.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import HomeRoute from "./home/home";
 import FortuneRoute from "./fortune/fortune";
 import ContainerRoute from "./contenedores/contenedores";
@@ -10,6 +10,7 @@ export default function RoutesManager(){
                 <Route path="/" element={<HomeRoute />}/>
                 <Route path="/proyects/fortune" element={<FortuneRoute />}/>
                 <Route path="/proyects/contenedores" element={<ContainerRoute />}/>
+                <Route path="*" element={<Navigate to="/" replace />}/>
             </Routes>
         </BrowserRouter>
     )
